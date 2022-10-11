77494b54e9e2d250c2227e2d3005384ca217ee7b
Ahmed Mgua <mguaahmed@gmail.com>
2022-10-11 19:03:47 +0200

    Create NetworkManager Swift Package from XCFramework

--- Package.swift
+++ Package.swift
@@ -18,11 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "NetworkManager",
-            dependencies: []),
-        .testTarget(
-            name: "NetworkManagerTests",
-            dependencies: ["NetworkManager"]),
+        .binaryTarget(name: "NetworkManager", path: "Sources/NetworkManager.xcframework")
     ]
 )
---
+++ Sources/NetworkManager.xcframework/ios-arm64/NetworkManagerSDK.framework/Headers/NetworkManagerSDK.h
@@ -0,0 +1,18 @@
+//
+//  NetworkManagerSDK.h
+//  NetworkManagerSDK
+//
+//  Created by Ahmed Mgua on 05/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkManagerSDK.
+FOUNDATION_EXPORT double NetworkManagerSDKVersionNumber;
+
+//! Project version string for NetworkManagerSDK.
+FOUNDATION_EXPORT const unsigned char NetworkManagerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkManagerSDK/PublicHeader.h>
+
+
